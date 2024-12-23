984ca6b5b86e49ebca57688ac611720816448435
s-celles <s.celles@gmail.com>
2024-12-23 10:39:51 +0100

    Initial version
    Signed-off-by: s-celles <s.celles@gmail.com>

---
+++ hello.cob
@@ -0,0 +1,13 @@
+      $set sourceformat(free)
+      *> Hello World COBOL program
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello.
+       
+       ENVIRONMENT DIVISION.
+       
+       DATA DIVISION.
+       
+       PROCEDURE DIVISION.
+           DISPLAY "Hello, World!".
+           STOP RUN.
+           
